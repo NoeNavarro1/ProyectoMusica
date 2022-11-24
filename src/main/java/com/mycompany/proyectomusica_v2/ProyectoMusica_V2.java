4214034971395c40ed47e43ea11ff260b1573aa4
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2022-11-23 22:05:44 -0600

    Inicializando proyecyo

---
+++ src/main/java/com/mycompany/proyectomusica_v2/ProyectoMusica_V2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyectomusica_v2;
+
+
+
+
+/**
+ *
+ * @author Noena
+ */
+public class ProyectoMusica_V2 {
+    
+
+    
+    public static void main(String[] args) {
+        
+       
+        
+    }
+}

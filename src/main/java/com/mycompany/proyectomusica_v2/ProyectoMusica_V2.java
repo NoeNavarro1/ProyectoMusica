32620759ae39a476749de50bc81c96a1cc6ad309
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2022-11-25 21:27:04 -0600

    Organizando mejor mis clases y metodos

--- src/main/java/com/mycompany/proyectomusica_v2/ProyectoMusica_V2.java
+++ src/main/java/com/mycompany/proyectomusica_v2/ProyectoMusica_V2.java
@@ -6,6 +6,7 @@ package com.mycompany.proyectomusica_v2;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,13 +19,14 @@ import java.io.PrintWriter;
  */
 public class ProyectoMusica_V2 {
 
+    //metodo que ayuda a poder leer archivos con extension .txt
     public static void leerArchivo(String name) {
         File archivo = null; //apunta a un archivo fisico de dd
         FileReader fr = null; //para leer el archivo 
         BufferedReader bufer = null;
         try {
             //creamos un apuntador al archivo físico
-            archivo = new File("C:\\Users\\Noena\\" + name + ".txt");
+            archivo = new File("C:\\Users\\Noena\\Artistas.txt");
             //abrimos el archivo para lectura
             fr = new FileReader(archivo);
             //configurar bufer para hacer la lectura
@@ -34,11 +36,11 @@ public class ProyectoMusica_V2 {
             String linea;
             //mientras haya informacion en el archivo
             while ((linea = bufer.readLine()) != null) {
-                System.out.println("Linea: " + linea);
+                System.out.println(linea);
             }
 
         } catch (Exception e) {
-            System.out.println("No se encuentra el archivo");
+            System.out.println("No se encuentra el archivo: " + name);
             e.printStackTrace();
         } finally {
             //esta cláusula se ejecuta siempre
@@ -49,14 +51,14 @@ public class ProyectoMusica_V2 {
                     fr.close();
                 }
             } catch (Exception e2) {
-                System.out.println("Error al cerrar el archivo");
+                System.out.println("Error al cerrar el archivo: " + name);
                 e2.printStackTrace();
             }
         }
 
     }
 
-
+    //Ayuda a la creacion de un Nuevo artista
     public static void escribirArchivo(String name) {
         FileWriter archivo = null;
         PrintWriter pw = null;
@@ -64,10 +66,10 @@ public class ProyectoMusica_V2 {
         String entrada;
         char respuesta;
         try {
-            archivo = new FileWriter("C:\\Users\\Noena\\" + name + ".txt");
+            archivo = new FileWriter("C:\\Users\\Noena\\Artistas.txt");
             pw = new PrintWriter(archivo);
             do {
-                System.out.println("Escribe informacion a guardar en el archivo: ");
+                System.out.println("Creacion del artista: ");
                 entrada = bufer2.readLine();
                 //Agrega lo leido en teclado al archivo en disco
                 pw.println(entrada);
@@ -93,94 +95,49 @@ public class ProyectoMusica_V2 {
         }
     }
 
-    public static void editarArchivo(String name) {
-        FileWriter archivo = null;
-        PrintWriter pw = null;
-        BufferedReader bufer2 = new BufferedReader(new InputStreamReader(System.in));
-        String entrada;
-        char respuesta;
+    public static void borrarDatos(String name) {
         try {
-            archivo = new FileWriter("C:\\Users\\Noena\\" + name + ".txt");
-            pw = new PrintWriter(archivo);
-            do {
-                System.out.println("Edita información del archivo: ");
-                entrada = bufer2.readLine();
-                //Agrega lo leido en teclado al archivo en disco
-                pw.println(entrada);
+            String path = "C:\\Users\\Noena\\Artistas.txt";
+            File archivo = new File(path);
 
-                System.out.println("Escribe s para continuar, n para parar");
-                entrada = bufer2.readLine();
-                respuesta = entrada.charAt(0);
-            } while (respuesta != 'n');
+            File archivoTemp = new File(archivo + ".tmp");
 
-        } catch (Exception e) {
-            System.out.println("Error al escribir el archivo: " + name);
-            e.printStackTrace();
-        } finally {
-            try {
-                //Cerrar el archivo i es que se pudo abrir para escritura
-                if (null != archivo) {
-                    archivo.close();
+            BufferedReader br = new BufferedReader(new FileReader(archivo));
+            PrintWriter pw = new PrintWriter(new FileWriter(archivoTemp));
+
+            String linea = null;
+
+            //Copia el contenido del archivo, menos la linea que se indica que se va a borrar
+            while ((linea = br.readLine()) != null) {
+                /*String.trim() método que borra los carácteres vacíos iniciales y finales de la cadena
+          Ejemplo: " Hola!" -> "Hola"
+                 */
+                if (!linea.trim().equals(name)) {
+
+                    pw.println(linea);
+                    //flush() cuando no queremos cerrar el archivo, pero sí vacear el bufer
+                    pw.flush();
                 }
-            } catch (Exception e2) {
-                System.out.println("Error al cerrar archivo" + name);
-                e2.printStackTrace();
             }
+            pw.close();
+            br.close();
+
+            //Validación de eliminar el archivo orginal
+            if (!archivo.delete()) {
+                System.out.println("No se pudo eliminar el archivo" + name);
+                return;
+            }
+
+            //Renombrar el archivo temporal como el archivo original
+            if (!archivoTemp.renameTo(archivo)) {
+                System.out.println("No se pudo renombrar el archivo" + name);
+            }
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-    }
-
-    public static void eliminarArchivo(String name) {
-        File archivo = new File("C:\\Users\\Noena\\" + name + ".txt");
-        if (archivo.delete()) {
-            System.out.println("Archivo eliminado " + archivo.getName());
-        } else {
-            System.out.println("Error al eliminar archivo ");
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader buferTeclado = new BufferedReader(new InputStreamReader(System.in));
-        String entrada;
-        String fileName;
-        int opcion;
-
-        System.out.println("Programa para escuchar canciones");
-        System.out.println("--------------------------------------------");
-        System.out.println("¿Que quieres hacer?");
-        System.out.println("1 -Seleccionar artista");
-        System.out.println("2 -Crear artista");
-        System.out.println("3 -Editar un artista");
-        System.out.println("4 -Eliminar un artista!!!!!");
-        entrada = buferTeclado.readLine();
-        opcion = Integer.parseInt(entrada);
-        fileName = entrada;
-        switch (opcion) {
-            case 1: {
-                //Lectura de un archivo de texto
-                System.out.println("Artistas: ");
-                Artistas.Artista(fileName);
-                System.out.println("Seleccionar artista:");
-                
-            }
-                break;
-            case 2: {
-                System.out.println("Creacion de un artista");
-                escribirArchivo(fileName);
-            }
-            break;
-            case 3: {
-                System.out.println("Funcion todavia no disponible");
-            }
-            break;
-            case 4: {
-                System.out.println("Artista eliminado correctamente");
-                eliminarArchivo(fileName);
-            }
-            break;
-            default:
-                System.out.println("Opcion no valida");
-        }
-
     }
 
 }
---
+++ src/main/java/com/mycompany/proyectomusica_v2/Test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectomusica_v2;
+
+/**
+ *
+ * @author Noena
+ */
+public class Test {
+    
+}
